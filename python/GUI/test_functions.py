1c85fff7d6af5b55dce86a7f0953b6348c7d75e9
timhot <timhot@hotmail.com>
2019-09-12 08:59:39 +1200

    Code to call tests

---
+++ python/GUI/test_functions.py
@@ -0,0 +1,18 @@
+import gui_functions
+
+# gui_functions.test_get_recording_information_for_a_single_recording()
+
+# gui_functions.test_update_recording_in_database()
+
+# gui_functions.test_update_recording_information_for_single_recording()
+
+
+# gui_functions.test_get_audio_recordings_with_tags_information_from_server()
+
+# gui_functions.test_get_and_store_tag_information_for_recording()
+
+# gui_functions.test_insert_tag_into_database()
+
+# gui_functions.test_get_unique_devices_stored_locally()
+
+gui_functions.test_get_all_tags_for_all_devices_in_local_database()
